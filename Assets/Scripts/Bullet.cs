02123e3e025ecc569d0d25467e13d9f911c8a0b1
Sinclair <sinfram248@gmail.com>
2019-04-10 15:53:16 +1000

    Initial Commit

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	float speed = 30f;
+	float angle = 90f;
+	float angleSpeed = 0f;
+
+	void Start() {
+	}
+
+	public void Move(float t) {
+		transform.position += new Vector3(Mathf.Cos(angle * 0.01745329f), 0f, Mathf.Sin(angle * 0.01745329f))*speed*t;
+		angle += angleSpeed * Time.deltaTime;
+	}
+
+	void Update () {
+		Move(Time.deltaTime);
+		if (transform.position.z > 22.5f || transform.position.z < -2.5f || transform.position.x > 10f || transform.position.x < -10f) {
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	float speed = 15f;
+	float fireRate = 10f;
+	public float timer = 0f;
+
+	public GameObject bulletPrefab;
+	GameObject bullet;
+
+	void Start() {
+		fireRate = 1f / fireRate;
+	}
+	
+	void Update () {
+		transform.position += new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"))*speed*Time.deltaTime;
+
+		timer -= Time.deltaTime;
+		if (Input.GetMouseButton(0)) {
+			while (timer <= 0f) {
+				timer += fireRate;
+				bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+				bullet.GetComponent<Bullet>().Move(fireRate-timer);
+			}
+		} else {
+			if (timer < 0f) {
+				timer = 0f;
+			}
+		}
+	}
+}
